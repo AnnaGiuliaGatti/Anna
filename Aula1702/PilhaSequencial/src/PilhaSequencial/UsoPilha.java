b6ee94ad64d78bfb78cce8927c23fef90e4880df
AnnaGiuliaGatti <61739327+AnnaGiuliaGatti@users.noreply.github.com>
2020-03-03 09:26:34 -0300

    Primeira versão
    Primeira versão

---
+++ Aula1702/PilhaSequencial/src/PilhaSequencial/UsoPilha.java
@@ -0,0 +1,23 @@
+package PilhaSequencial;
+
+public class UsoPilha {
+
+	public static void main(String[] args) {
+		PilhaInt pilha = new PilhaInt();
+		
+		pilha.init();
+		
+		pilha.push(10);
+		pilha.push(12);
+		pilha.push(22);
+		pilha.push(33);
+		pilha.push(44);
+		pilha.push(55);
+		pilha.push(66);
+		pilha.push(77);
+		
+		System.out.println("Valor retirado: " + pilha.pop());
+		System.out.println("Valor retirado: " + pilha.pop());
+	}
+
+}
